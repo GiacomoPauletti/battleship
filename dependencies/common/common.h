2cc14d4238f7b5051cf5399436ee55458dcae26b
giacomo <giacomo.pauletti@gmail.com>
2021-11-28 18:19:59 +0100

    Moved stuff to share between multiple codes in common

---
+++ dependencies/common/common.h
@@ -0,0 +1,19 @@
+#include "../map/map.h"
+#include "../ship/ship.h"
+
+#if !defined(COMMON_H)
+#define COMMON_H
+#define MAX_NAME_LENGTH 20
+/* maximum length of player name */
+
+typedef struct
+{
+    char name[MAX_NAME_LENGTH + 1];
+    MapWrap defenceMap;
+    MapWrap attackMap;
+
+    Ship ships[MAX_SHIP_NUM];
+    int shipNumber;
+
+} Player;
+#endif
