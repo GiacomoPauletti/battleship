fea7ae94bdea4b2ae5962a760a9a170c691d8da3
giacomo <giacomo.pauletti@gmail.com>
2021-12-18 20:17:43 +0100

    Added FileData and FolderData

--- dependencies/database/database.h
+++ dependencies/database/database.h
@@ -5,7 +5,7 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-#define MAX_PATH_LENGTH 50
+#define MAX_PATH_LENGTH 70
 #define DATA_PATH "data"
 /* database path */
 #define SETTINGS_PATH "settings"
@@ -18,6 +18,34 @@
 #else
     #define NULL_FILE "nul"
 #endif
+
+#define MAX_FILE_NAME_LEN 20
+#define MAX_FILES 10
+#define MAX_FOLDERS 10
+
+typedef struct fileData
+{
+    char name[MAX_FILE_NAME_LEN];
+    char path[MAX_PATH_LENGTH];
+    /* absolute path
+     * contains file name */
+
+} FileData;
+
+typedef struct folderData
+{
+    char name[MAX_FILE_NAME_LEN];
+
+    /* files inside this folder */
+    int numFiles;
+    FileData files[MAX_FILES];
+
+    /* folders inside this folder */
+    int numFolders;
+    struct folderData folders[MAX_FOLDERS];
+
+} FolderData;
+
 
 int createDatabase();
 /* creates all the database */
